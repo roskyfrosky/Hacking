d77dc160eef9a17a66402f73fbfb6c853875f074
Oscar <oskar2212@gmail.com>
2014-09-28 16:46:56 +0200

    argparser added & out to file

--- spyder.py
+++ spyder.py
@@ -1,14 +1,32 @@
 #! /bin/python
 from bs4 import BeautifulSoup
-import urllib2,urllib,sys, argparse
+import urllib2,urllib,sys,argparse
+
+parser =argparse.ArgumentParser()
+parser.add_argument("url", help="Input the url to Spyder")
+parser.add_argument("-o","--output",help="Output the result to a file", action="store_true")
+args=parser.parse_args()
+
 
 def search(d):
 	content= urllib2.urlopen(d).read()
 	soup= BeautifulSoup(content)
 	for link in soup.find_all('a',href=True):	
-		aux.append(link['href'])
+		string= d+link['href'] +"\n"
+		if i==1 :	
+			f.write(string)
+		else:		
+			print string
+def main():
+	global i ,f
+	i=0	
+	url=sys.argv[len(sys.argv)-1] #url start
+	if args.output:
+		f=open("OutSpyder.txt","a")
+		i=1
+	search(url)
+	if i==1:
+		f.close()
 
-url=sys.argv[1]
-aux=[]
-search(url)
-print aux
+if __name__=="__main__":
+	main()
