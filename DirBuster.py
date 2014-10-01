d4b50b6efa6be1e3049c428de744afc2fb20be1b
Oscar <oskar2212@gmail.com>
2014-10-01 12:36:56 +0200

    Initial Version
    With this script you can found the default paths

---
+++ DirBuster.py
@@ -0,0 +1,30 @@
+#! /bin/python
+from bs4 import BeautifulSoup
+import urllib,sys,argparse,httplib
+
+parser =argparse.ArgumentParser()
+parser.add_argument("url", help="Input the url")
+parser.parse_args()
+
+HiddenDir= ["/system/", "/manager/","/admin/","/administrator/"]
+found=[]
+def main():
+	i=0
+	while i < len (HiddenDir): 
+		url = sys.argv[len(sys.argv)-1]
+		end = HiddenDir[i]
+		con=httplib.HTTPConnection(url)	
+		con.request('GET',end)
+		res =con.getresponse()
+		if res.status == 200 :
+			found.append(url+end)					
+			
+				
+		i+=1	
+	con.close()	
+
+	for x in found:
+		print x
+
+if __name__=="__main__":
+	main()
