e2e22b904ed76408f7d713f2452c7092083d26ac
Oscar <oskar2212@gmail.com>
2014-10-02 14:22:35 +0200

    Input file added

--- DirBuster.py
+++ DirBuster.py
@@ -1,29 +1,27 @@
 #! /bin/python
 from bs4 import BeautifulSoup
-import urllib,sys,argparse,httplib
+import sys,argparse,httplib
 
 parser =argparse.ArgumentParser()
 parser.add_argument("url", help="Input the url")
+parser.add_argument("i",help="Input dictionary file")
 parser.add_argument("-f","--FileOut",help="Output the result to a file", action="store", dest="outfile")
-result=parser.parse_args()
+result = parser.parse_args()
 
-HiddenDir= ["/system/", "/manager/","/admin/","/administrator/"]
 found=[]
 def main():
-	i=0
-	while i < len (HiddenDir): 
-		url = sys.argv[len(sys.argv)-1]
-		end = HiddenDir[i]
-		con=httplib.HTTPConnection(url)	
-		con.request('GET',end)
+	inp = sys.argv[len(sys.argv)-1]
+	op= open(inp,"r")
+	for line in op:
+		url = sys.argv[len(sys.argv)-2]
+		con=httplib.HTTPConnection(url)		
+		end=str("/"+line.strip("\n")+"/")
+		con.request('GET',end)		
 		res =con.getresponse()
 		if res.status == 200 :
-			found.append(url+end)					
-			
-				
-		i+=1	
-	con.close()	
-
+			found.append(url+end)				
+	con.close()
+	
 	if result.outfile != None:
 		f = open (result.outfile,"a");
 		for x in found:
